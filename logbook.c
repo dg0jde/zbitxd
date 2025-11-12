cd7a5cccd88e174d895d1d82ed5925c9b2eed434
Shawn Rutledge <s@ecloud.org>
2025-11-12 19:30:40 +0100

    Fix buffer overflow in logbook_add 10 characters is not enough for date, because it ends with a null character. But 5 is enough for time: it's only HHmm format so far. Found with ASAN (the -fsanitize=address flag in Makefile).

--- logbook.c
+++ logbook.c
@@ -299,7 +299,7 @@ void message_add(char *mode, unsigned int frequency, int outgoing, char *message
 
 void logbook_add(char *contact_callsign, char *rst_sent, char *exchange_sent, 
 	char *rst_recv, char *exchange_recv){
-	char statement[1000], *err_msg, date_str[10], time_str[10];
+	char statement[1000], *err_msg, date_str[11], time_str[5];
 	char freq[12], log_freq[12], mode[10], mycallsign[12];
 
 	time_t log_time = time(NULL);
