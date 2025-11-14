53fd0a05597a3c50f2af12beebaffb4010011dd3
Shawn Rutledge <s@ecloud.org>
2025-11-14 15:20:58 +0100

    Ensure that we never send an empty R report It seems the usual sequence is supposed to be ft8_message_tokenize(): 	strcpy(signal_strength, p); ft8_on_start_qso():     field_set(SENT, signal_strength); ft8_process(): 	report_send = field_str("SENT"); ft8_on_signal_report(): 	sprintf(reply_message, "%s %s R%s", call, mycall, report_send);
    But what if ft8_on_start_qso() didn't get called? I've seen cases where
    I send out a useless packet like "A1XYZ LB2JK R"
    If sbitx receives such a packet, it treats it as R+00; but maybe most
    software will see it as invalid. I doubt such QSOs get logged.
    So make sure that if "SENT" is not set, we set it to the signal strength
    of the last packet (so that it's visible in the GTK SENT field before
    we actually start sending the packet).

--- modem_ft8.c
+++ modem_ft8.c
@@ -864,7 +864,8 @@ float ft8_next_sample(){
 static char m1[32], m2[32], m3[32], m4[32], signal_strength[10], mygrid[10],
 	reply_message[100];
 static int rx_pitch, tx_pitch, confidence_score, msg_time;
-static const char *call, *exchange, *report_send, *report_received, *mycall;
+static const char *call = NULL, *exchange = NULL,
+    *report_send = NULL, *report_received = NULL, *mycall = NULL;
 
 int ft8_message_tokenize(char *message){
 	char *p;
@@ -960,6 +961,7 @@ void ft8_on_start_qso(char *message){
 			field_set("EXCH", m3);
 			field_set("SENT", signal_strength);
 		}
+		LOG(LOG_DEBUG, "ft8_on_start_qso CQ: rst s %s\n", signal_strength);
 		sprintf(reply_message, "%s %s %s", call, mycall, mygrid);
 	}
 	//whoa, someone cold called us
@@ -968,6 +970,7 @@ void ft8_on_start_qso(char *message){
 	    get_field_value_by_label("CALL", cur_call);
 		set_call_field(m2);
 		field_set("SENT", signal_strength);
+		LOG(LOG_DEBUG, "ft8_on_start_qso cold call: rst s %s\n", signal_strength);
 		//they might have directly sent us a signal report
 		if (isalpha(m3[0]) && isalpha(m3[1]) && strncmp(m3,"RR",2)!=0){ // R- RR are not EXCH
 			field_set("EXCH", m3);
@@ -1005,6 +1008,11 @@ void ft8_on_signal_report(){
 	}
 	else{
 		field_set("RECV", m3);
+		// in case ft8_on_start_qso() was not called: ensure that we send some numeric signal report
+		if (!field_str("SENT")[0]) {
+			field_set("SENT", signal_strength);
+			report_send = field_str("SENT");
+		}
 		sprintf(reply_message, "%s %s R%s", call, mycall, report_send);
 		ft8_tx(reply_message, tx_pitch);
 	}
